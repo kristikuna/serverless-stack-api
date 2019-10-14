11ae4825b9147a1e46b0acebff7b3908b781966d
Byrnes, Kristi M <kristi.m.byrnes@accenture.com>
2019-10-14 15:20:24 -0400

    Add serverless API

---
+++ update.js
@@ -0,0 +1,25 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main (event, context) {
+	const data = JSON.parse(event.body);
+	const params = {
+		TableName: process.env.tableName,
+		Key: {
+			userId: event.requestContext.identity.cognitoIdentityId,
+			noteId: event.pathParameters.id
+		},
+		UpdateExpression: "SET content = :content, attachment = :attachment",
+		ExpressionAttributeValues: {
+			":attachment": data.attachment || null,
+			":content": data.content || null
+		},
+		ReturnValues: 'ALL_NEW'
+	};
+	try {
+		await dynamoDbLib.call("update", params);
+		return success({ status: true });
+	} catch (e) {
+		return failure({ status: false });
+	}
+}
